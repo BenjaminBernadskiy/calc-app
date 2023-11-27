7f42f333a1901ac1beb638e5c57038bb0da9b2b7
BenjaminBernadskiy <144385265+BenjaminBernadskiy@users.noreply.github.com>
2023-11-27 12:32:02 -0500

    e

--- main.py
+++ main.py
@@ -1,86 +1,32 @@
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
 
 root = tk.Tk()
-root.title("Simple Calculater")
-root.geometry("300x400")
+root.title("Notepad")
+root.geometry("600x400")
 
-display_frame = tk.Frame(root, width=300, height=50, bg="gray")
-display_frame.pack()
+menu_frame = tk.Frame(root)
+menu_frame.pack(fill="x")
 
-display = tk.Entry(display_frame,font=("Ariel", 20) ,insertwidth=4, width=14, justify="center")
-display.pack(padx=10, pady=30)
+text_frame = tk.Frame(root)
+text_frame.pack(fill="both", expand=True)
 
-def update_display(value):
-    current = display.get()
-    display.delete(0, tk.END)
-    display.insert(tk.END, current+value)
+menu = tk.Menu(menu_frame)
 
-buttons_frame = tk.Frame(root)
-buttons_frame.pack()
-
-for i in range(1, 10):
-    btn = tk.Button(buttons_frame, text=str(i), padx=20, pady=10, command=lambda i=i: update_display(str(i)))
-    btn.grid(row=2 + (i-1) // 3, column=(i-1) % 3)
-
-btnzero = tk.Button(buttons_frame, text="0", padx=20, pady=10, command=lambda i=i: update_display("0"))
-btnzero.grid(row=5, column=0)
-
-btndot = tk.Button(buttons_frame, text=".", padx=20, pady=10, command=lambda i=i: update_display("."))
-btndot.grid(row=5, column=1)
-
-
-def add():
+def new_note():
     pass
 
-def subtract():
-    pass
+new_icon = tk.PhotoImage(file="new.png")
+open_icon = tk.PhotoImage(file="open.png")
+close_icon = tk.PhotoImage(file="close.png")
+save_icon = tk.PhotoImage(file="save.png")
 
-def multiply():
-    pass
+file_menu = tk.Menu(menu, tearoff=0)
+file_menu.add_command(label="New",image=new_icon, compound=tk.LEFT, command=new_note)
 
-def divide():
-    pass
+root.config(menu=menu)
 
-btn_add = tk.Button(buttons_frame, text="+", padx=20, pady=10, command=add)
-btn_add.grid(row=2, column=3)
-
-btn_add = tk.Button(buttons_frame, text="-", padx=20, pady=10, command=subtract)
-btn_add.grid(row=3, column=3)
-
-btn_add = tk.Button(buttons_frame, text="*", padx=20, pady=10, command=multiply)
-btn_add.grid(row=4, column=3)
-
-btn_add = tk.Button(buttons_frame, text="/", padx=20, pady=10, command=divide)
-btn_add.grid(row=5, column=3)
-
-def square():
-    pass
-
-def inverse():
-    pass
-
-def negate():
-    pass
-
-btn_square = tk.Button(buttons_frame, text="x^2", padx=20, pady=10, command=square)
-btn_square.grid(row=1, column=1)
-
-btn_inverse = tk.Button(buttons_frame, text="1/x", padx=20, pady=10, command=inverse)
-btn_inverse.grid(row=1, column=2)
-
-btn_negate = tk.Button(buttons_frame, text="+/-", padx=20, pady=10, command=negate)
-btn_negate.grid(row=1, column=3)
-
-def clear():
-    pass
-
-def equal():
-    pass
-
-btn_clear = tk.Button(buttons_frame, text="C", padx=20, pady=10, command=clear)
-btn_clear.grid(row=1, column=0)
-
-btn_equal = tk.Button(buttons_frame, text="=", padx=20, pady=10, command=equal)
-btn_equal.grid(row=5, column=2)
+text_area = ScrolledText(text_frame, wrap=tk.WORD)
+text_area.pack(fill="both")
 
 root.mainloop()
